26760c7b8e53c3215d86e70ced63a1ee9de2711f
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-08 16:23:51 +0000

    refactored formCtrl and saved users by cohort name

--- app/js/controllers/formctrl.js
+++ app/js/controllers/formctrl.js
@@ -1,35 +1,19 @@
-dailyReviewApp.controller('FormCtrl', ['$scope', '$firebaseArray', '$rootScope', '$filter',
-  function($scope, $firebaseArray, $rootScope, $filter) {
+dailyReviewApp.controller('FormCtrl', ['$scope', '$rootScope', '$filter',
+  function($scope, $rootScope, $filter) {
 
       var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
 
-      var juniorsRef = ref.child("juniors");
-      var seniorsRef = ref.child("seniors");
-
       $scope.addReview = function() {
-        if($scope.cohort === 'Junior') {
-            juniorsRef.child($rootScope.userEmail || $rootScope.userName).push({
-              name: $rootScope.userEmail || $rootScope.userName,
-              cohort: $scope.cohort,
-              pairing: $scope.pairing,
-              challenge: $scope.challenge,
-              feeling: $scope.feeling,
-              confidence: $scope.confidence,
-              comments: $scope.comments,
-              date: $filter('date')(new Date(), 'dd-MM-yy')
-          });
-        } else {
-            seniorsRef.child($rootScope.userEmail || $rootScope.userName).push({
-              name: $rootScope.userEmail || $rootScope.userName,
-              cohort: $scope.cohort,
-              pairing: $scope.pairing,
-              challenge: $scope.challenge,
-              feeling: $scope.feeling,
-              confidence: $scope.confidence,
-              comments: $scope.comments,
-              date: $filter('date')(new Date(), 'dd-MM-yy')
-            });
-        }
+        ref.child($scope.cohort).child($rootScope.userEmail || $rootScope.userName).push({
+          name: $rootScope.userEmail || $rootScope.userName,
+          cohort: $scope.cohort,
+          pairing: $scope.pairing,
+          challenge: $scope.challenge,
+          feeling: $scope.feeling,
+          confidence: $scope.confidence,
+          comments: $scope.comments,
+          date: $filter('date')(new Date(), 'dd-MM-yy')
+        });
           $scope.name = '';
           $scope.pairing = '';
           $scope.challenge = '';
