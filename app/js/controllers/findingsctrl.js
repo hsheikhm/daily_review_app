6757701b710b9eacee62a75d1710217e58695715
Chuka Ebi <chuka.ebi@outlook.com>
2015-12-09 13:59:19 +0000

    DOne

--- app/js/controllers/findingsctrl.js
+++ app/js/controllers/findingsctrl.js
@@ -1,75 +1,74 @@
 dailyReviewApp.controller('FindingsCtrl', ['$scope', '$firebaseObject', '$firebaseArray',
-  function($scope, $firebaseObject, $firebaseArray) {
+function($scope, $firebaseObject, $firebaseArray) {
 
-      var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
-      var usersRef = ref.child();
+  var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
 
-      var reviews = $firebaseObject(ref);
+  var reviews = $firebaseObject(ref);
 
-      reviews.$bindTo($scope, "users");
-      console.log(reviews);
-      $scope.users = [];
+  reviews.$bindTo($scope, "users");
 
+  $scope.users = [];
 
 
 
-      // reviews.$loaded(function(data) {
-      //   angular.forEach(data, function(value, key) {
+
+  // reviews.$loaded(function(data) {
+    //   angular.forEach(data, function(value, key) {
       //     angular.forEach(value, function(val, ky) {
-      //       $scope.users.push(val);
-      //     });
-      //   });
-      // });
+        //       $scope.users.push(val);
+        //     });
+        //   });
+        // });
 
 
-      // ref.on('child_added', function(childSnapshot, prevChildKey) {
-      //   console.log(childSnapshot);
-      // });
+        // ref.on('child_added', function(childSnapshot, prevChildKey) {
+          //   console.log(childSnapshot);
+          // });
 
 
-  }]);
+        }]);
 
-  // $scope.getAverage = function(array) {
-  //   var total = 0;
-  //   for(var i = 0; i < array.length; i++) {
-  //     total += array[i].pairing;
-  //   }
-  //   var avg = total / array.length;
-  //   return avg;
-  // };
+        // $scope.getAverage = function(array) {
+          //   var total = 0;
+          //   for(var i = 0; i < array.length; i++) {
+            //     total += array[i].pairing;
+            //   }
+            //   var avg = total / array.length;
+            //   return avg;
+            // };
 
-  // fullDataObj.$bindTo($scope, "data");
+            // fullDataObj.$bindTo($scope, "data");
 
-  // $scope.seniors = 'Oct 2015';
-  // $scope.juniors = 'Nov 2015';
-  // $scope.cohorts = [];
-  // $scope.juniorChallengeRatings = [];
-  // $scope.seniorChallengeRatings = [];
-  // $scope.juniorFeelingRatings = [];
-  // $scope.seniorFeelingRatings = [];
-  // $scope.juniorPairingRatings = [];
-  // $scope.seniorPairingRatings = [];
-  //
-  // $scope.users = [{pairing: 1}, {pairing: 5}, {pairing: 2}, {pairing: 3}, {pairing: 4} ];
+            // $scope.seniors = 'Oct 2015';
+            // $scope.juniors = 'Nov 2015';
+            // $scope.cohorts = [];
+            // $scope.juniorChallengeRatings = [];
+            // $scope.seniorChallengeRatings = [];
+            // $scope.juniorFeelingRatings = [];
+            // $scope.seniorFeelingRatings = [];
+            // $scope.juniorPairingRatings = [];
+            // $scope.seniorPairingRatings = [];
+            //
+          // $scope.users = [{pairing: 1}, {pairing: 5}, {pairing: 2}, {pairing: 3}, {pairing: 4} ];
 
 
-  //
-  // fullDataObj.$bindTo($scope, "data").then(function() {
-  //   angular.forEach($scope.data.cohorts, function(value, key) {
-  //     $scope.cohorts.push(key);
-  //     angular.forEach(value, function(v, k) {
-  //       angular.forEach(v, function(vl, ky) {
-  //         // console.log(vl);
-  //         if(vl.cohort === $scope.juniors) {
-  //           $scope.juniorChallengeRatings.push(vl.challenge);
-  //           $scope.juniorFeelingRatings.push(vl.feeling);
-  //           $scope.juniorPairingRatings.push(vl.pairing);
-  //         } else {
-  //           $scope.seniorChallengeRatings.push(vl.challenge);
-  //           $scope.seniorFeelingRatings.push(vl.feeling);
-  //           $scope.seniorPairingRatings.push(vl.pairing);
-  //         }
-  //       });
-  //     });
-  //   });
-  // });
+          //
+          // fullDataObj.$bindTo($scope, "data").then(function() {
+            //   angular.forEach($scope.data.cohorts, function(value, key) {
+              //     $scope.cohorts.push(key);
+              //     angular.forEach(value, function(v, k) {
+                //       angular.forEach(v, function(vl, ky) {
+                  //         // console.log(vl);
+                  //         if(vl.cohort === $scope.juniors) {
+                    //           $scope.juniorChallengeRatings.push(vl.challenge);
+                    //           $scope.juniorFeelingRatings.push(vl.feeling);
+                    //           $scope.juniorPairingRatings.push(vl.pairing);
+                    //         } else {
+                      //           $scope.seniorChallengeRatings.push(vl.challenge);
+                      //           $scope.seniorFeelingRatings.push(vl.feeling);
+                      //           $scope.seniorPairingRatings.push(vl.pairing);
+                      //         }
+                      //       });
+                      //     });
+                      //   });
+                      // });
--- app/js/controllers/formctrl.js
+++ app/js/controllers/formctrl.js
@@ -4,7 +4,7 @@ dailyReviewApp.controller('FormCtrl', ['$scope', '$rootScope', '$filter',
       var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
 
       $scope.addReview = function() {
-        ref.child($rootScope.userName).push({
+        ref.push({
           name: $rootScope.userName,
           cohort: $scope.cohort,
           pairing: $scope.pairing,
