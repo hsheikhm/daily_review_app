25d170790bbc22c695e7a2bbe140087ef4c5e44a
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-11 17:19:54 +0000

    refactored loginctrl

--- app/js/controllers/formctrl.js
+++ app/js/controllers/formctrl.js
@@ -1,25 +1,25 @@
 dailyReviewApp.controller('FormCtrl', ['$scope', '$rootScope', '$filter',
   function($scope, $rootScope, $filter) {
 
-      var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
+    var ref = new Firebase("https://shining-fire-9962.firebaseio.com");
 
-      $scope.addReview = function() {
-        ref.push({
-          name: $rootScope.userName,
-          cohort: $scope.cohort,
-          pairing: $scope.pairing,
-          pairingExtra: $scope.pairingExtra || '',
-          challenge: $scope.challenge,
-          challengeExtra: $scope.challengeExtra || '',
-          feeling: $scope.feeling,
-          feelingExtra: $scope.feelingExtra || '',
-          confidence: $scope.confidence,
-          comments: $scope.comments || '',
-          date: $filter('date')(new Date(), 'dd-MM-yy')
-        });
-      };
+    $scope.addReview = function() {
+      ref.push({
+        name: $rootScope.userName,
+        cohort: $scope.cohort,
+        pairing: $scope.pairing,
+        pairingExtra: $scope.pairingExtra || '',
+        challenge: $scope.challenge,
+        challengeExtra: $scope.challengeExtra || '',
+        feeling: $scope.feeling,
+        feelingExtra: $scope.feelingExtra || '',
+        confidence: $scope.confidence,
+        comments: $scope.comments || '',
+        date: $filter('date')(new Date(), 'dd-MM-yy')
+      });
+    };
 
-      $scope.thankYou = function() {
-          $scope.done = true;
-      };
-    }]);
+    $scope.thankYou = function() {
+        $scope.done = true;
+    };
+}]);
--- app/js/controllers/loginctrl.js
+++ app/js/controllers/loginctrl.js
@@ -10,29 +10,33 @@ dailyReviewApp.controller('LoginCtrl', ['$scope', '$location', '$rootScope',
       }, function(error, authData) {
         if (error) {
           console.log("Login Failed!", error);
-        } else {
-          if ($scope.email === "admin@makers.com") {
-            console.log("Authenticated successfully with payload:", authData);
-            $scope.$apply(function() { $location.path("/login-success-path-coach"); });
-          } else {
-            console.log("Authenticated successfully with payload:", authData);
-            $scope.$apply(function() { $location.path("/login-success-path-student"); });
-            $rootScope.userEmail = authData.password.email;
-          }
-        }
+        } else { $scope.allowLogin(authData); }
       });
     };
 
+    $scope.allowLogin = function(authData) {
+      if ($scope.makersAdmin()) {
+        console.log("Authenticated successfully with:", authData);
+        $scope.$apply(function() { $location.path("/login-success-path-coach"); });
+      } else {
+        console.log("Authenticated successfully with:", authData);
+        $scope.$apply(function() { $location.path("/login-success-path-student"); });
+      }
+    };
+
+    $scope.makersAdmin = function() {
+      return $scope.email === "admin@makers.com";
+    };
 
     $scope.loginGitHub = function() {
-        ref.authWithOAuthPopup("github", function(error, authData) {
-        if (error) {
-          console.log("Login Failed!", error);
-        } else {
-          console.log("Authenticated successfully with payload:", authData.github.displayName);
-          $scope.$apply(function() { $location.path("/login-success-path-student"); });
-          $rootScope.userName = authData.github.displayName;
-        }
-      });
-    };
-  }]);
+      ref.authWithOAuthPopup("github", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with:", authData.github.displayName);
+        $scope.$apply(function() { $location.path("/login-success-path-student"); });
+        $rootScope.userName = authData.github.displayName;
+      }
+    });
+  };
+}]);
