97a172020192cd7cd0563ae9ccfa55ff6eb362b9
Hamza Sheikh <hsheikhm@gmail.com>
2015-12-11 18:49:10 +0000

    finished refactoring student-form.html and created directives for form

--- app/js/directives/formQuestionsDirective.js
+++ app/js/directives/formQuestionsDirective.js
@@ -1,13 +1,41 @@
 dailyReviewApp.directive("cohortQuestion", function() {
   return {
     restrict: 'E',
-    templateUrl: "/public/views/cohort-question.html"
+    templateUrl: "/views/cohort-question.html"
   };
 });
 
 dailyReviewApp.directive("pairingQuestion", function() {
   return {
     restrict: 'E',
-    templateUrl: '/public/views/pairing-question.html'
+    templateUrl: '/views/pairing-question.html'
+  };
+});
+
+dailyReviewApp.directive("challengeQuestion", function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/challenge-question.html'
+  };
+});
+
+dailyReviewApp.directive("confidenceQuestion", function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/confidence-question.html'
+  };
+});
+
+dailyReviewApp.directive("feelingQuestion", function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/feeling-question.html'
+  };
+});
+
+dailyReviewApp.directive("commentsQuestion", function() {
+  return {
+    restrict: 'E',
+    templateUrl: '/views/comments-question.html'
   };
 });
